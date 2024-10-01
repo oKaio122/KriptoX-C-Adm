2d0f92da74f0ef742de4e315d6ef17ac8f64871e
oKaio122 <123851567+oKaio122@users.noreply.github.com>
2024-10-01 11:48:00 -0300

    feat: Adicionado mensagem de boas vindas ao logar [+] adicionado variavel para armazenar boas_vindas com o nome do usuario [+] codigo para juntar nome do usuario com boas vindas [+] funcao mostrar_menu com boas vindas como nome_menu

--- main.c
+++ main.c
@@ -66,7 +66,6 @@ void menu_opcoes(User usuarios[10], int pos, Cotacoes *cotacao){
     snprintf(boas_vindas, sizeof(boas_vindas), "Bem-vindo de volta, %s!", usuarios[pos].nome);
 
     system("cls||clear");
-
     mostrar_menu(boas_vindas);
 
 // Menu de opcoes

652dcd5f634a195341a8eb14c4f596fb1506487b
oKaio122 <kposansky2@gmail.com>
2024-11-16 00:31:54 -0300

    feat: funcao de consultar saldo do adm - parecida com consultar extrato adm, puxa usuario pelo cpf - printa saldo de todas as moedas do usuario - removido print desncessario do cadastrar criptomoeda (era debug)

--- kutils.h
+++ kutils.h
@@ -89,3 +89,4 @@ void mostrar_info_user(char titulo[], User usuario);
 void criar_moeda(Moeda *moedas, int pos_moeda, char *nome, double cotacao, double taxa_compra, double taxa_venda);
 void adicionar_moeda_users(User usuarios[], char *nome_moeda);
 void remover_moeda_users(User usuarios[], char *nome_moeda);
+void consultar_saldo_admin(User usuarios[]);
